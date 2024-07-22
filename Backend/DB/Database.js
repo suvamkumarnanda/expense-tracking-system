7ec922b7919adf06dcb0bfa1b4ba74970227ca19
Suvam Kumar Nanda <68044124+suvamkumarnanda@users.noreply.github.com>
2024-07-22 12:05:07 +0530

    Add files via upload

---
+++ Backend/DB/Database.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+import dotenv from 'dotenv'
+dotenv.config()
+export const connectDB = async (req, res) => {
+    const db = process.env.MONGO_URL;
+
+    const {connection} = await mongoose.connect(db, { useNewUrlParser: true });
+
+    console.log(`MongoDB Connected to ${connection.host}`);
+
+}
